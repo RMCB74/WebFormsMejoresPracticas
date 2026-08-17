7658472393a8e80f513b0d5d4f9da59d7b993620
Dell-PC <cndcorreo@gmail.com>
2026-08-17 13:56:54 -0600

    Agrega pruebas unitarias para servicios

---
+++ WebFormsSoapService.Tests/Fakes/ClienteRepositoryFake.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using WebFormsSoapService.Interfaces;
+using WebFormsSoapService.Models;
+
+
+
+namespace WebFormsSoapService.Tests.Fakes
+{
+    public class ClienteRepositoryFake : IClienteRepository
+    {
+        //public Cliente ObtenerPorId(int id)
+        //{
+        //    return null;
+        //}
+
+        public Cliente ObtenerPorId(int id)
+        {
+            return new Cliente
+            {
+                Id = id,
+                Nombre = "Cliente Fake"
+            };
+        }
+        public List<Cliente> ObtenerTodos()
+        {
+            return new List<Cliente>();
+        }
+
+        public int Crear(Cliente cliente)
+        {
+            return 1;
+        }
+
+        public bool Actualizar(Cliente cliente)
+        {
+            return true;
+        }
+
+        public bool Eliminar(int id)
+        {
+            return true;
+        }
+    }
+}
---
+++ WebFormsSoapService.Tests/Fakes/ContactoRepositoryFake.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using WebFormsSoapService.Interfaces;
+using WebFormsSoapService.Models;
+
+namespace WebFormsSoapService.Tests.Fakes
+{
+    public class ContactoRepositoryFake : IContactoRepository
+    {
+        public Contacto ObtenerPorId(int id)
+        {
+            return new Contacto
+            {
+                Id = id,
+                ClienteId = 1,
+                TipoContactoId = 1,
+                Valor = "fake@prueba.com"
+            };
+        }
+
+        public List<Contacto> ObtenerTodos()
+        {
+            return new List<Contacto>();
+        }
+
+        //public List<Contacto> ObtenerPorCliente(int clienteId)
+        //{
+        //    return new List<Contacto>();
+        //}
+
+        public List<Contacto> ObtenerPorCliente(int clienteId)
+        {
+            return new List<Contacto>
+            {
+                new Contacto
+                {
+                    Id = 1,
+                    ClienteId = clienteId,
+                    TipoContactoId = 1,
+                    Valor = "fake@prueba.com"
+                },
+                new Contacto
+                {
+                    Id = 2,
+                    ClienteId = clienteId,
+                    TipoContactoId = 2,
+                    Valor = "5551234567"
+                }
+            };
+        }
+
+
+        public int Crear(Contacto contacto)
+        {
+            return 1;
+        }
+
+        public bool Actualizar(Contacto contacto)
+        {
+            return true;
+        }
+
+        public bool Eliminar(int id)
+        {
+            return true;
+        }
+    }
+}
